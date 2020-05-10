cea6e59a6a8d15b18cf5ee34ed0c2133ace673b7
The-Hoko <62822359+The-Hoko@users.noreply.github.com>
2020-05-09 23:13:46 -0300

    Add files via upload

---
+++ Mercado/Produto.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Mercado
+{
+    class Produto
+    {
+        private string _nome;
+        public double  Preco { get;  set; }
+        public int Quantidade { get;  set; }
+
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Proprerties 
+        public string Nome
+        {
+            get 
+            { 
+                return _nome; 
+            }
+
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdiconarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        // Sobreposição
+        // Override indica que a operação  vem de outra classe
+
+        public override string ToString()
+        {
+            return _nome
+                + ", R$: "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: R$: "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ Mercado/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Produto produto = new Produto("TV", 500.00, 10);
+
+                 
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+            Console.WriteLine("Dados dos produtos: " + produto);
+
+            // Adicionar itens ao estoque
+            Console.WriteLine();
+            Console.Write("Digite q quantidade de produtos a ser adiconado em estoque: ");
+            int Adicionar_Quantidade = int.Parse(Console.ReadLine());
+            produto.AdiconarProdutos(Adicionar_Quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
+            
+            // Remove itens do estoque 
+            Console.WriteLine();
+            Console.Write("Digite a quantidade que deseja remover do estoque: ");
+            int Remover_Quantidade = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(Remover_Quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
+            
+           
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Quantidade);
+            
+            Console.ReadKey();
+        }
+    }
+}
